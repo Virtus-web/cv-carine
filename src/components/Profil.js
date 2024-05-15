44701c1578fbc581ec2a0269d359b43891160e8f
Virtus-web <nicolas.pedenau@hotmail.fr>
2024-05-15 13:58:25 +0200

    profil

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -4,11 +4,10 @@ function Profil() {
         <div className="profil mb5">
             <h2>Profil | En poste, à l'écoute du marché</h2>
             <p style={{textAlign: 'justify'}}>
-                Après une reconversion dans le domaine du développement informatique en 2020,
-                j'ai tout d'abord orienté mes projets dans un cadre indépendant afin de consolider ma formation.
-                Par ailleurs, je me suis positionné sur le marché du travail ce qui m'a permis d'obtenir un CDI chez un éditeur.
-                Cette expérience m'a fait acquérir de nouvelles compétences en aduéquation avec les demandes du marché.
-                Aujourd'hui, toujours en poste, j'ai la volonté de rejoindre une entreprise dynamique ayant des projets attractifs.
+                Forte d’une expérience de 16 ans dans le milieu bancaire,
+                j’ai accompagné de nombreux professionnels dans leur projet de création ou de développement de leur entreprise.
+                Aujourd'hui, je souhaite mettre ma personnalité et mes compétences au service de ma créativité,
+                dans le but d'accomplir mon propre projet professionnel : devenir architecte d'intérieur.
             </p>
         </div>
     )
