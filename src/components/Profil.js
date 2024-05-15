acf2f264b358fe7c89d5c21ffb23524326b6782e
Virtus-web <nicolas.pedenau@hotmail.fr>
2024-05-15 22:30:35 +0200

    meta title

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -2,7 +2,7 @@ function Profil() {
     
     return (
         <div className="profil mb5">
-            <h2>Profil | En poste, à l'écoute du marché</h2>
+            <h2>Profil | En reconversion professionnelle</h2>
             <p style={{textAlign: 'justify'}}>
                 Forte d’une expérience de 16 ans dans le milieu bancaire,
                 j’ai accompagné de nombreux professionnels dans leur projet de création ou de développement de leur entreprise.
